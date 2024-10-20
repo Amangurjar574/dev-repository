3e25a024e03acc59a0e7c3bac33dc1d51d8be988
Aman Gurjar <amangurjar4466@gmail.com>
2024-10-20 15:29:31 +0530

    update jar file

--- src/main/java/com/springboot/swt/project/utilities/Permision.java
+++ src/main/java/com/springboot/swt/project/utilities/Permision.java
@@ -4,6 +4,6 @@ public class Permision {
 
     public static boolean BATCH_PERMIT = false;
 //    	public static String redirectLink = "https://d970cd84-6da2-4608-9620-6f7cf53d725c.e1-us-east-azure.choreoapps.dev";
-    	public static String redirectLink = "https://db3acb4c-abab-4425-a4f8-142191a9696a.e1-us-east-azure.choreoapps.dev";
-//    public static String redirectLink = "";
+//    	public static String redirectLink = "https://db3acb4c-abab-4425-a4f8-142191a9696a.e1-us-east-azure.choreoapps.dev";
+    public static String redirectLink = "https://1054d209-0768-4f6b-a191-e24923ec93a5.e1-us-east-azure.choreoapps.dev";
 }
